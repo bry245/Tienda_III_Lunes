73b17a6b6170c408f440317f8b0d5158f94731b9
vinda <vinda@192.168.1.9>
2022-09-15 00:06:22 -0600

    Paso1

---
+++ src/main/java/com/Tienda_3QL/Tienda3QlApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_3QL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tienda3QlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tienda3QlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tienda_3QL/Tienda3QlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_3QL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tienda3QlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
